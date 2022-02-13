b499b4d156ac52aed2cf8f07c283876ca6f01342
Ben Wright <ben31w@gmail.com>
2022-02-13 12:37:08 -0500

    work on methods in step 5

--- src/food_item.py
+++ src/food_item.py
@@ -10,3 +10,28 @@ class FoodItem:
 
     def __str__(self):
         return f"{self.food_type}, calories: {self.calories}, fat: {self.fat}, taste score: {self.taste_score}"
+
+    def increment_calories(self, calories):
+        self.calories += calories
+
+    @staticmethod
+    def get_most_caloric_food(food_items):
+        if food_items.length == 0:
+            return None
+
+        most_caloric_food = food_items[0]
+        for food in food_items:
+            if food.calories > most_caloric_food.calories:
+                most_caloric_food = food
+        return most_caloric_food
+
+    @staticmethod
+    def get_least_fatty_food(food_items):
+        if food_items.length == 0:
+            return None
+
+        least_fatty_food = food_items[0]
+        for food in food_items:
+            if food.fat < least_fatty_food.calories:
+                least_fatty_food = food
+        return least_fatty_food
